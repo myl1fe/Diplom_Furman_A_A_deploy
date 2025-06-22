27ebaf7d21ba4a453ebc2d0d281c7b5bf4d88908
myl1fe <snake220v@gmail.com>
2025-06-22 18:39:45 +0300

    fix host v3 db v5

--- app/wsgi.py
+++ app/wsgi.py
@@ -13,4 +13,15 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
+
+
+
+
+# Проверка выполнения миграций
+MIGRATE_FLAG = os.path.join(os.path.dirname(__file__), '.migrated')
+if not os.path.exists(MIGRATE_FLAG):
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'migrate'])
+    open(MIGRATE_FLAG, 'w').close()
+
 application = get_wsgi_application()
